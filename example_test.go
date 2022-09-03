72fe7d9529ecd7cd34f7f11a8fe9d5ae56f6e5fb
Hidetake Iwata <int128@gmail.com>
2022-09-04 08:39:14 +0900

    Add examples (#15)

--- example_test.go
+++ example_test.go
@@ -19,6 +19,15 @@ func ExampleTimeRange_Contains() {
 	// output: The reservation at 2006-01-02 15:06:00 +0000 UTC is available.
 }
 
+func ExampleNew() {
+	r := timerange.New(
+		time.Date(2006, 1, 2, 15, 4, 5, 0, time.UTC),
+		time.Date(2006, 1, 2, 15, 7, 5, 0, time.UTC),
+	)
+	fmt.Print(r)
+	// output: [2006-01-02T15:04:05Z, 2006-01-02T15:07:05Z]
+}
+
 func ExampleNewFrom() {
 	r := timerange.NewFrom(
 		time.Date(2006, 1, 2, 15, 4, 5, 0, time.UTC),
@@ -26,4 +35,22 @@ func ExampleNewFrom() {
 	)
 	fmt.Print(r)
 	// output: [2006-01-02T15:04:05Z, 2006-01-02T15:19:05Z]
+}
+
+func ExampleNewUntil() {
+	r := timerange.NewUntil(
+		time.Date(2006, 1, 2, 15, 4, 5, 0, time.UTC),
+		15*time.Minute,
+	)
+	fmt.Print(r)
+	// output: [2006-01-02T14:49:05Z, 2006-01-02T15:04:05Z]
+}
+
+func ExampleTimeRange_String() {
+	r := timerange.New(
+		time.Date(2006, 1, 2, 15, 4, 5, 0, time.UTC),
+		time.Date(2006, 1, 2, 15, 7, 5, 0, time.UTC),
+	)
+	fmt.Print(r)
+	// output: [2006-01-02T15:04:05Z, 2006-01-02T15:07:05Z]
 }
