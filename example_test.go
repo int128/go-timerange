24ae241c456ac4f1943b87e0ccc6386f2649b396
Hidetake Iwata <int128@gmail.com>
2022-09-03 17:26:10 +0900

    Add example (#5)

---
+++ example_test.go
@@ -0,0 +1,20 @@
+package timerange_test
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/int128/go-timerange"
+)
+
+func ExampleTimeRange_Contains() {
+	desiredTime := time.Date(2006, 1, 2, 15, 6, 0, 0, time.UTC)
+	availableRange := timerange.TimeRange{
+		Start: time.Date(2006, 1, 2, 15, 4, 5, 0, time.UTC),
+		End:   time.Date(2006, 1, 2, 15, 7, 5, 0, time.UTC),
+	}
+	if availableRange.Contains(desiredTime) {
+		fmt.Printf("The reservation at %s is available.", desiredTime)
+	}
+	// output: The reservation at 2006-01-02 15:06:00 +0000 UTC is available.
+}
