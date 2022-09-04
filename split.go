8dc4fb107fc49e9ed489d3b93144408c77b13d83
Hidetake Iwata <int128@gmail.com>
2022-09-04 14:18:02 +0900

    Add TimeRange.SplitIterator()

--- split.go
+++ split.go
@@ -11,3 +11,43 @@ func (r TimeRange) Split(span time.Duration) []time.Time {
 	}
 	return points
 }
+
+// SplitIterator returns an iterator for time points within this range.
+// If span is longer than this range, it returns only start time.
+//
+// Consider this method instead of Split, if the result is too long.
+func (r TimeRange) SplitIterator(span time.Duration) SplitIterator {
+	return &splitIterator{timeRange: r, span: span}
+}
+
+// SplitIterator is an iterator to retrieve time points within a range.
+type SplitIterator interface {
+	// HasNext returns true if the next time is within the range.
+	HasNext() bool
+
+	// Next returns the next time.
+	// It the next time is not in the range, this returns a zero value.
+	Next() time.Time
+}
+
+type splitIterator struct {
+	next      time.Time
+	timeRange TimeRange
+	span      time.Duration
+}
+
+func (s *splitIterator) HasNext() bool {
+	return s.next.Equal(s.timeRange.end) || s.next.Before(s.timeRange.end)
+}
+
+func (s *splitIterator) Next() time.Time {
+	if !s.HasNext() {
+		return time.Time{}
+	}
+	current := s.next
+	if current.IsZero() {
+		current = s.timeRange.start
+	}
+	s.next = current.Add(s.span)
+	return current
+}
